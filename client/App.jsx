85e9bd1f01ea7376c551b4a0be3cde359aa2c349
Will Hughes <killjoyfpss@wills-mbp.lan>
2022-04-08 16:00:47 -0500

    Here is the addition of the landing page

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Technique from "./pages/Technique.jsx";
+import Landing from "./pages/landing.jsx";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Technique" element={<Technique/>} />
+                <Route path="/Landing" element={<Landing/>} />
             </Routes>
         </BrowserRouter>
     )
---
+++ client/pages/landing.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./landing.css" 
+const Landing = () => {
+
+    return (
+<>
+
+
+<div id="Water">
+
+
+    <img id="Water_drop" src={require("../assets/Water_drop.svg")} />
+
+    
+<div id="button">
+<button type="button" class="btn btn-link">Enter</button>
+
+</div>
+</div>
+</>
+
+
+    );
+}
+
+
+export default Landing;
